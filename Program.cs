2f1f1cc6221f23321d3665b1fbd7d4e48a40908b
sandy-tartari <62676992+sandy-tartari@users.noreply.github.com>
2020-06-23 13:52:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Faça um algoritmo que diga se o número é par digtado é par ou impar
+            int n;
+            Console.WriteLine ("Escreva um número qualquer: (e tecle ENTER)");
+            n = int.Parse(Console.ReadLine());
+
+            if ((n % 2) == 0){
+                Console.WriteLine("É par");
+            }
+            else {
+                Console.WriteLine("É impar");
+            }
+ 
+        }
+    }
+}
